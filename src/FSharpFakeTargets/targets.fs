3139733b14174809c43b796415e191b281eaafba
Andrew William Seward <awswrd@gmail.com>
2016-03-24 21:17:30 -0700

    Start creating ConfigParams type

---
+++ src/FSharpFakeTargets/targets.fs
@@ -0,0 +1,18 @@
+﻿namespace DatNET
+
+module Targets =
+  open Fake
+  open Fake.FileSystem
+  open System.IO
+
+  let private RootDir = Directory.GetCurrentDirectory()
+
+  type ConfigParams =
+    {
+      SolutionFile : FileIncludes
+    }
+
+  let ConfigDefaults() =
+    {
+      SolutionFile = !! (Path.Combine(RootDir, "*.sln"))
+    }
