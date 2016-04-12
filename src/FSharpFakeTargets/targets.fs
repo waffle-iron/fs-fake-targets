39bbf84645a349864af88beffa0298679f5d4c98
Mathew Glodack <glodack@gmail.com>
2016-04-11 20:54:46 -0700

    Create basic build/clean fake targets

--- src/FSharpFakeTargets/targets.fs
+++ src/FSharpFakeTargets/targets.fs
@@ -10,9 +10,34 @@ module Targets =
   type ConfigParams =
     {
       SolutionFile : FileIncludes
+      MSBuildArtifacts : FileIncludes
     }
 
   let ConfigDefaults() =
     {
       SolutionFile = !! (Path.Combine(RootDir, "*.sln"))
+      MSBuildArtifacts = !! "**/bin/**.*" ++ "**/obj/**.*"
     }
+
+  let private _CreateTarget targetName parameters targetFunc =
+    Target targetName targetFunc
+    parameters
+
+  let private _MSBuildTarget parameters =
+    _CreateTarget "MSBuild" parameters (fun _ ->
+        parameters.SolutionFile
+            |> MSBuildRelease null "Build"
+            |> ignore
+    )
+
+  let private _CleanTarget parameters =
+    _CreateTarget "Clean" parameters (fun _ ->
+         CleanDirs parameters.MSBuildArtifacts
+    )
+
+  let Initialize setParams =
+    let parameters = ConfigDefaults() |> setParams
+
+    parameters
+        |> _MSBuildTarget
+        |> _CleanTarget
